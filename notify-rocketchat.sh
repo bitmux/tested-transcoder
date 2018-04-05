ee0e8353e2dcf4d3dd31e294689c424e85b591f1
bitmux <firdman1@gmail.com>
2018-04-05 21:03:47 +0000

    Comments

--- notify-rocketchat.sh
+++ notify-rocketchat.sh
@@ -5,10 +5,12 @@ if [ ! -f /etc/notify-rocketchat.conf ]; then
     echo "Please create config file"
     set -e
  fi
-source /etc/notify-rocketchat.conf
+
+# config file containing assignments for user, url, and token must be placed in /etc/
+source /etc/notify-rocketchat.conf 
 
 #message="$(</dev/stdin)"
-while read message; do
+while read message; do #its easier to handle piping to json if I don't do multiple lines, hence one line at a time because I suck at coding.
 
 #  /usr/bin/curl -s -X POST -H 'Content-Type: application/json' --data "$(request_body)" "$url/$token"
   /usr/bin/curl -s POST -H 'Content-Type: application/json' --data '{ "username":"'$user'","text":"'"$message"'"}' "$url/$token"
@@ -16,13 +18,13 @@ while read message; do
 
 done
 
-request_body()
-{
-  cat <<EOF
-{
-  "username": "$user",
-  "message": "$message",
-    }
-EOF
-}
+#request_body()
+#{
+#  cat <<EOF
+#{
+#  "username": "$user",
+#  "message": "$message",
+#    }
+#EOF
+#}
 
